17ede54a44b49607dcfb8397d18fa565847082d7
niccolonardelli <niccolo.nardelli@marconibari.edu.it>
2025-11-26 19:14:01 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+float calcolarea(float r)
+{
+    float area;
+    area = M_PI * r *r;
+    return area;
+}
+float calcolacirconferenza (float r)
+{
+    float circonferenza;
+    circonferenza = 2*r*M_PI;
+    return circonferenza;
+}
+void main()
+{
+    float circonferenza, area, r1, r2;
+    cout << "inserisci i valori dei raggi di due cerchi"<< endl;
+    cin>>r1>>r2;
+    area = calcolarea(r1);
+    circonferenza = calcolacirconferenza(r1);
+    cout << "l'area del cerchio del primo raggio ha un valore di " << area << endl;
+    cout << "la circonferenza del cerchio del primo raggio ha un valore di " << circonferenza << endl;
+    area = calcolarea(r2);
+    circonferenza = calcolacirconferenza(r2);
+    cout << "l'area del cerchio del secondo raggio ha un valore di " << area << endl;
+    cout << "la circonferenza del cerchio del secondo raggio ha un valore di " << circonferenza << endl;
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
